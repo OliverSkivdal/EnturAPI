da49403da8e553ae9d9798cb81c2fd0df24b46fc
OliverSkivdal <63880823+OliverSkivdal@users.noreply.github.com>
2021-05-03 12:26:52 +0200

    Fiks for tresifrede linjenummer

--- Start.sh
+++ Start.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 clear
 python3 API.py > .respons.txt
-cat .respons.txt |sed 's/\:[0-9][0-9]+0[1-2]:00//g' |sed 's/\[/ /g' |sed 's/\,/\n/g' |sed 's/\]/\n/g' |sed s/\'//g |sed s/\{//g |sed 's/destinasjon://g' |sed s/\}//g |sed 's/sanntid//g' |sed 's/:[^~]*T//g' |sed 's/ : //g' |sed -r '/^\s*$/d' |sed ':a;N;$!ba;s/\n/ /g' |sed 's/^.\{6\}//g' |sed 's/\linje:/\n/g' |sed 's/: / /g' > .output.txt
+cat .respons.txt |sed 's/\:[0-9][0-9]+0[1-2]:00//g' |sed 's/\[/ /g' |sed 's/\,/\n/g' |sed 's/\]/\n/g' |sed s/\'//g |sed s/\{//g |sed 's/destinasjon://g' |sed s/\}//g |sed 's/sanntid//g' |sed 's/:[^~]*T//g' |sed 's/ : //g' |sed -r '/^\s*$/d' |sed ':a;N;$!ba;s/\n/ /g' |sed 's/^.\{6\}//g' |sed 's/\linje:/\n/g' |sed 's/: / /g' |sed 's/   /\t/g' > .output.txt
 echo 
 echo Klokken er:
 date +%R
